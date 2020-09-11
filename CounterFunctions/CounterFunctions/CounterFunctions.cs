fc7181f2d3fefe4bbdfb99b13119697f914870cf
aubaida <n_b_a_09@hotmail.com>
2020-09-11 20:33:52 +0300

    added limitation

--- CounterFunctions/CounterFunctions/CounterFunctions.cs
+++ CounterFunctions/CounterFunctions/CounterFunctions.cs
@@ -46,151 +46,7 @@ namespace CounterFunctions
                    log.LogError(ex, "Failed to negotiate connection.");
                    throw;
                }
-           }/*
-           //addition
-           //public static CloudTable table;
-           //end_addition
-           [FunctionName("update-counter")]
-           public static async Task UpdateCounter(
-               [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequestMessage request,
-               [Table("CountersTable")] CloudTable cloudTable,
-               [SignalR(HubName = "CounterHub")] IAsyncCollector<SignalRMessage> signalRMessages,
-               ILogger log)
-           {
-               //addition
-               CloudTable table=null;
-               try
-               {
-                   StorageCredentials creds = new StorageCredentials(Environment.GetEnvironmentVariable("accountName"), Environment.GetEnvironmentVariable( "accountKey"));
-                   CloudStorageAccount account = new CloudStorageAccount(creds, useHttps: true);
-
-                   CloudTableClient client = account.CreateCloudTableClient();
-
-                   table = client.GetTableReference("counters");
-                   await table.CreateIfNotExistsAsync();
-
-                   Console.WriteLine(table.Uri.ToString());
-               }
-               catch (Exception ex)
-               {
-                   Console.WriteLine(ex);
-               }
-              // table = new CloudTable(siteURL);
-               //end_addition
-               log.LogInformation("Updating counter.");
-
-               Counter counterRequest = await ExtractContent<Counter>(request);
-
-               Counter cloudCounter = await GetOrCreateCounter(table, counterRequest.Id);
-               cloudCounter.Count++;
-               log.LogInformation("************the cloud counter ID="+ cloudCounter.Id+" ,count="+ cloudCounter.Count);
-               ConnectionRequest connectionRequest = await ExtractContent<ConnectionRequest>(request);
-               TableOperation updateOperation = TableOperation.InsertOrReplace(cloudCounter);
-               await table.ExecuteAsync(updateOperation);
-
-               await signalRMessages.AddAsync(
-                   new SignalRMessage
-                   {
-                       Target = "CounterUpdate",
-                       Arguments = new object[] { cloudCounter }
-                   });
            }
-
-           [FunctionName("get-counter")]
-           public static async Task<Counter> GetCounter(
-               [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "get-counter/{id}")] HttpRequestMessage request,
-               [Table("CountersTable")] CloudTable cloudTable,
-               string id,
-               ILogger log)
-           {
-               log.LogInformation("Getting counter.");
-               //addition
-               CloudTable table = null;
-               try
-               {
-                   StorageCredentials creds = new StorageCredentials(Environment.GetEnvironmentVariable("accountName"), Environment.GetEnvironmentVariable("accountKey"));
-                   CloudStorageAccount account = new CloudStorageAccount(creds, useHttps: true);
-
-                   CloudTableClient client = account.CreateCloudTableClient();
-
-                   table = client.GetTableReference("counters");
-                   await table.CreateIfNotExistsAsync();
-
-                   Console.WriteLine(table.Uri.ToString());
-               }
-               catch (Exception ex)
-               {
-                   Console.WriteLine(ex);
-               }
-               // table = new CloudTable(siteURL);
-               //end_addition
-               return await GetOrCreateCounter(table, int.Parse(id));
-           }
-
-           private static async Task<T> ExtractContent<T>(HttpRequestMessage request)
-           {
-               string connectionRequestJson = await request.Content.ReadAsStringAsync();
-               Console.Out.Write("the connectionRequestJson is :   "+connectionRequestJson +"\n");
-               return JsonConvert.DeserializeObject<T>(connectionRequestJson);
-           }
-
-           private static async Task<Counter> GetOrCreateCounter(CloudTable cloudTable, int counterId)
-           {
-               TableQuery<Counter> idQuery = new TableQuery<Counter>()
-                   .Where(TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, counterId.ToString()));
-            //   Console.Out.Write("111111111111111111111111111111\n");
-               TableQuerySegment<Counter> queryResult = await cloudTable.ExecuteQuerySegmentedAsync(idQuery,null);
-           //    Console.Out.Write("12222222222222222222222222222\n");
-               Counter cloudCounter = queryResult.FirstOrDefault();
-           //    Console.Out.Write("3333333333333333333333333333333333333333\n");
-               if (cloudCounter == null)
-               { 
-                   cloudCounter = new Counter { Id = counterId };
-
-                   TableOperation insertOperation = TableOperation.InsertOrReplace(cloudCounter);
-                   cloudCounter.PartitionKey = "counter";
-                   cloudCounter.RowKey = cloudCounter.Id.ToString();
-                   TableResult tableResult = await cloudTable.ExecuteAsync(insertOperation);
-                   return await GetOrCreateCounter(cloudTable, counterId);
-               }
-
-               return cloudCounter;
-           }
-           [FunctionName("get-devices")]
-           public static async Task<Device[]> getDevices(
-               [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequestMessage request,
-               [Table("CountersTable")] CloudTable cloudTable,
-               [SignalR(HubName = "CounterHub")] IAsyncCollector<SignalRMessage> signalRMessages,
-               ILogger log)
-           {
-               log.LogInformation("getting the devices.");
-               List<string> devices = new List<string>();
-
-               //add
-               registryManager = RegistryManager.CreateFromConnectionString(connectionString);
-               IQuery query = registryManager.CreateQuery("SELECT * FROM devices", 100);
-               while (query.HasMoreResults)
-               {
-                   var page = await query.GetNextAsTwinAsync();
-                   foreach (var twin in page)
-                   {
-                       Console.Out.Write("the twin DeviceId :" + twin.DeviceId + "\n");
-                       devices.Add(twin.DeviceId);
-                   }
-               }
-               //end_add
-               Device[] retList = new Device[devices.Count];
-               int index = 0;
-               foreach (string s in devices) {
-                   Device d = new Device();
-                   d.id = s;
-                   retList[index]=d;
-                   index++;
-               }
-               return retList;
-
-           }
-   */
         [FunctionName("get-isOpen")]
         public static async Task<status> GetIfIsOpen(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "get-isOpen/{id}/{state}")] HttpRequestMessage request,
@@ -271,6 +127,12 @@ namespace CounterFunctions
                      Target = ((Request)result.Result).UserName+"Notify",
                      Arguments = new[] { ((Request)result.Result).ownerName+" left the garage" }
                  });
+                await signalRMessages.AddAsync(
+                 new SignalRMessage
+                 {
+                     Target = "adminNotify",
+                     Arguments = new[] { ((Request)result.Result).ownerName + " left the garage" }
+                 });
 
                 TableOperation add = TableOperation.InsertOrReplace(places);
                 await garageTable.ExecuteAsync(add);
@@ -320,6 +182,12 @@ namespace CounterFunctions
                      Target = regesterdUser + "Notify",
                      Arguments = new[] { ownerName + " entered the garage" }
                  });
+                    await signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    Target = "adminNotify",
+                    Arguments = new[] { ownerName + " entered the garage" }
+                });
                     TableOperation addOrReplace = TableOperation.InsertOrReplace(places);
                     await garageTable.ExecuteAsync(addOrReplace);
                     return st;
@@ -372,6 +240,7 @@ namespace CounterFunctions
             if (act.Equals("remove"))
             {
                 Console.Out.WriteLine("in remove");
+                //delete table
                 await table.DeleteIfExistsAsync();
                 //delete user from Users
                 TableOperation retrieve = TableOperation.Retrieve<TableEntity>(name, "");
@@ -384,6 +253,18 @@ namespace CounterFunctions
                 TableOperation delete = TableOperation.Delete(deleteEntity);
 
                 await usersTable.ExecuteAsync(delete);
+                //delete requests
+                CloudTable requestTable = client.GetTableReference("Requests");
+                TableQuery<Request> idQuery = new TableQuery<Request>()
+               .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, name));
+                TableQuerySegment<Request> queryResult = await requestTable.ExecuteQuerySegmentedAsync(idQuery, null);
+                var batchOperation = new TableBatchOperation();
+                foreach (var e in queryResult.Results) {
+                    batchOperation.Delete((TableEntity)e);
+                }
+                if((queryResult.Results).Count != 0) { 
+                    await requestTable.ExecuteBatchAsync(batchOperation);
+                }
 
                 return act + " " + name;
 
@@ -569,8 +450,7 @@ namespace CounterFunctions
            string user,
            ILogger log)
         {
-            log.LogInformation("GetRegesteredCar");
-            //addition
+            
             CloudTable table = null;
             CloudTableClient client = null;
             //first get all users
@@ -604,9 +484,6 @@ namespace CounterFunctions
             List<User> cars = new List<User>();
             foreach (string regesterdUser in users)
             {
-                /* if (regesterdUser.Equals("admin")) {
-                     continue;
-                 }*/
                 CloudTable usersTable = client.GetTableReference("Table00" + regesterdUser);
                 await usersTable.CreateIfNotExistsAsync();
                 TableQuery<User> idQuery = new TableQuery<User>();
@@ -837,6 +714,36 @@ namespace CounterFunctions
             }
             if (act.Equals("add"))
             {
+                //check if it's a new request 
+                List<Request> requestList = await GetRequests(table, user, true, null);
+                string idQuery = TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, plateNum);
+                TableQuery<Request> query = new TableQuery<Request>().Where(idQuery);
+                var results = await table.ExecuteQuerySegmentedAsync<Request>(query, null);
+                foreach (Request e in results.Results) {
+                    if (e.approved.Equals("waiting")) {
+                        if (e.PartitionKey.Equals(user))
+                        {
+                            return "Failed : the Request arleady suggested ";
+                        }
+                        else {
+                            return "Failed : the Request arleady suggested by another user";
+                        }
+                    }
+                }
+                //check if the car arleady regestered 
+                List<User> cars = await GetRegesteredCar(null,"admin",null);
+                foreach (User e in cars)
+                {
+                    if (e.RowKey.Equals(plateNum)) {
+                        if (e.UserName.Equals(user))
+                            {
+                            return "Failed : the car arleady registered by you ";
+                        }else{
+                            return "Failed : the car arleady registered by another user ";
+                        }
+                    }
+                    
+                }
                 Request newRequest = new Request();
                 newRequest.PartitionKey = user;
                 newRequest.RowKey = plateNum;
@@ -931,7 +838,7 @@ namespace CounterFunctions
                  new SignalRMessage
                  {
                      Target = user + "NotifyFeeds",
-                     Arguments = new[] { "" }
+                     Arguments = new[] { "emptyMassage" }
                  });
             if (act.Equals("true"))
             { //add the car
